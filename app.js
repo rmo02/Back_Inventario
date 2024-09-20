1a2305236820580edbe557bde58685a4123e8fa3
VictorEmanuel08 <victorem3181@gmail.com>
2024-09-20 10:50:52 -0300

    Habilitando CORS

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const { sequelize } = require('./models');
-const shelfRoutes = require('./routes/shelfRoutes');
-const sectionRoutes = require('./routes/sectionRoutes');
-const equipmentRoutes = require('./routes/equipmentRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cors = require("cors"); // Importa a biblioteca cors
+const { sequelize } = require("./models");
+const shelfRoutes = require("./routes/shelfRoutes");
+const sectionRoutes = require("./routes/sectionRoutes");
+const equipmentRoutes = require("./routes/equipmentRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 
 const app = express();
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(cors()); // Adiciona o middleware CORS
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(express.json());
-app.use('/api/shelves', shelfRoutes);
-app.use('/api/section', sectionRoutes);
-app.use('/api/equipment', equipmentRoutes);
-app.use('/api/category', categoryRoutes);
+app.use("/api/shelves", shelfRoutes);
+app.use("/api/section", sectionRoutes);
+app.use("/api/equipment", equipmentRoutes);
+app.use("/api/category", categoryRoutes);
 
 // Iniciar a sincronização com o banco de dados e o servidor
 sequelize.sync().then(() => {
-  app.listen(3000, () => console.log('Server running on port 3000'));
+  app.listen(3000, () => console.log("Server running on port 3000"));
 });
